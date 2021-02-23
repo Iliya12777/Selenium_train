fc9bcd600a2736f2558509b4a1a1fb994f2df324
Iliya <79529002+Iliya12777@users.noreply.github.com>
2021-02-23 20:29:51 +0200

    firstTest final commit, ready2be_checked by trainer

--- FirstHomework/firstTest.py
+++ FirstHomework/firstTest.py
@@ -4,19 +4,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 class MyFirstTestCase(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(self):
+    def setUp(self):
         self.emulatedBrowser = webdriver.Firefox()
-        wait = WebDriverWait(self.emulatedBrowser, 10)
 
-    @classmethod
-    def tearDownClass(self):
+    def tearDown(self):
         self.emulatedBrowser.quit()
 
     def test01_navigate_and_search(self):
         '''
         First test to try GitHub + PyCharm + PyUnit
         '''
+        findField = False
         with self.emulatedBrowser as ff:
             ff.get('https://google.com')
             findField = ff.find_element_by_name('q')
